e559f2217070701a808f50bafcbf120e79fe2375
Prateek Jindal <Prateek.Jindal@ibm.com>
2021-07-27 21:24:34 +0530

    working copy function.

--- 09-io/demo-02.go
+++ 09-io/demo-02.go
@@ -6,41 +6,78 @@ import (
 	"os"
 )
 
-type AlphaReader string
+type AlphaReader struct {
+	str   string
+	count int64
+}
 
-func (alphaReader AlphaReader) Read(p []byte) (n int, err error) {
-	fmt.Println("Buffer size ", len(p))
+const (
+	SeekStart   = 0 // seek relative to the origin of the file
+	SeekCurrent = 1 // seek relative to the current offset
+	SeekEnd     = 2 // seek relative to the end
+)
+
+func (alphaReader *AlphaReader) Read(p []byte) (n int, err error) {
 	count := 0
-	for idx := 0; idx < len(alphaReader); idx++ {
-		if (alphaReader[idx] >= 'A' && alphaReader[idx] <= 'Z') || (alphaReader[idx] >= 'a' && alphaReader[idx] <= 'z') {
-			p[count] = alphaReader[idx]
+	l, _ := alphaReader.Seek(0, SeekCurrent)
+
+	for idx := int(l); idx < len(alphaReader.str); idx++ {
+		if (alphaReader.str[idx] >= 'A' && alphaReader.str[idx] <= 'Z') ||
+			(alphaReader.str[idx] >= 'a' && alphaReader.str[idx] <= 'z') {
+			p[count] = alphaReader.str[idx]
 			count++
 			if count >= len(p) {
+				alphaReader.Seek(int64(idx), SeekStart)
 				return count, nil
 			}
 		}
 	}
+	alphaReader.Seek(0, SeekEnd)
 	return count, io.EOF
 }
 
-func copy(writer io.Writer, reader io.Reader) {
+func (alphaReader *AlphaReader) Seek(offset int64, whence int) (int64, error) {
+	length := int64(0)
+	maxLen := int64(len(alphaReader.str))
+	switch whence {
+	case SeekStart:
+		length = offset
+	case SeekCurrent:
+		if offset == 0 {
+			return alphaReader.count, io.EOF
+		} else {
+			length = alphaReader.count + offset
+		}
+	case SeekEnd:
+		length = maxLen - offset
+	}
+	if length < maxLen {
+		alphaReader.count = length
+	} else {
+		alphaReader.count, length = maxLen, maxLen
+	}
+	return length, io.EOF
+}
+
+func copy(writer io.Writer, reader io.ReadSeeker) {
 	totalBytesRead := 0
 	for {
 		buffer := make([]byte, 10)
 		bytesRead, err := reader.Read(buffer)
 
 		totalBytesRead += bytesRead
-		fmt.Println("Total bytes read ", totalBytesRead)
+		// fmt.Println("\nTotal bytes read ", totalBytesRead)
 		if bytesRead > 0 {
 			writer.Write(buffer[:bytesRead])
 		}
 		if err == io.EOF {
+			fmt.Println()
 			break
 		}
 	}
 }
 
 func main() {
-	alphaReader := AlphaReader("this is a sample string with 1234 numbers and *&^%^$ special characters")
-	copy(os.Stdout, alphaReader)
+	alphaReader := AlphaReader{"this is a sample string with 1234 numbers and *&^%^$ special characters", 0}
+	copy(os.Stdout, &alphaReader)
 }
